264573dff70c500de4cef14ec420c94991ec2e74
Your Name <hyymy9177@gmail.com>
2024-08-20 12:17:11 +0300

    fix console

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post('/fetch-metadata', async (req, res) => {
   res.json(metadataResults)
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+const port = process.env.PORT || 3000
+app.listen(port, () =>
+    console.log(`Server listening on port http://127.0.0.1:${port}/`)
+)
